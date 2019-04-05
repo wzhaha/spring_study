bb870a5b54b087a6b6cd2ca867f3d12cf0f0978a
wzhaha <35536677+wzhaha@users.noreply.github.com>
2019-04-05 20:57:10 +0800

    actuator

---
+++ actuator-server/src/main/java/com/admin/server/ServerApplication.java
@@ -0,0 +1,17 @@
+package com.admin.server;
+
+import de.codecentric.boot.admin.server.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@EnableAutoConfiguration
+@EnableAdminServer
+public class ServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ServerApplication.class, args);
+    }
+
+}
